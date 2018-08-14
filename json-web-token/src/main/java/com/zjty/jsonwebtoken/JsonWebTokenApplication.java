77ebc167f80182331972762f8d468943f363c883
xhyrzldf <xhyrzldf@gmail.com>
2018-08-14 12:01:07 +0800

    jwt模块完成

--- json-web-token/src/main/java/com/zjty/jsonwebtoken/JsonWebTokenApplication.java
+++ json-web-token/src/main/java/com/zjty/jsonwebtoken/JsonWebTokenApplication.java
@@ -8,31 +8,59 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.GenericFilterBean;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
 /**
  * jwt 示例教程
  * 参考文章 https://segmentfault.com/a/1190000009231329#articleHeader6
+ * 身份验证流程
+ * <li>1.{@link JWTLoginFilter#attemptAuthentication(HttpServletRequest, HttpServletResponse)}
+ * 解析用户对象,获得用户名和密码</li>
+ * <li>2.{@link CustomAuthenticationProvider#authenticate(Authentication)}验证用户名和密码</li>
+ * <li>3.验证成功后执行{@link JWTLoginFilter#successfulAuthentication(HttpServletRequest, HttpServletResponse, FilterChain, Authentication)}逻辑</li>
+ * <li>4.第三部逻辑中调用{@link TokenAuthenticationService#addAuthentication(HttpServletResponse, String)}方法生成jwt token返回</li>
  */
 @SpringBootApplication
 public class JsonWebTokenApplication {
@@ -45,10 +73,11 @@ public class JsonWebTokenApplication {
 
 @RestController
 class UserController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello JWT!");
     }
+
 }
 
 
@@ -62,6 +91,60 @@ class User {
     private String username;
 
     private String password;
+}
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 设置 HTTP 验证规则
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 关闭csrf验证
+        http.cors().and().csrf().disable()
+                // 对请求进行认证
+                .authorizeRequests()
+                // 所有 / 的所有请求 都放行
+                .antMatchers("/").permitAll()
+                // 所有 /login 的POST请求 都放行
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+//                // 添加权限检测
+//                .antMatchers("/hello").hasAuthority("AUTH_WRITE")
+//                // 角色检测
+//                .antMatchers("/world").hasRole("ADMIN")
+                // 所有请求需要身份认证
+                .anyRequest().authenticated()
+                .and()
+                // 添加一个过滤器 所有访问 /login 的请求交给 JWTLoginFilter 来处理 这个类处理所有的JWT相关内容
+                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+                // 添加一个过滤器验证其他请求的Token是否合法
+                .addFilterBefore(new JWTAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+    }
+
+//    /**
+//     * 跨域请求
+//     */
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("*"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+//        configuration.setAllowedHeaders(Arrays.asList("Origin","X-Requested-With"," Content-Type"," Accept","Authorization"));
+//        configuration.setAllowCredentials(true);
+//        configuration.setMaxAge(3600L);
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        // 使用自定义身份验证组件
+        auth.authenticationProvider(new CustomAuthenticationProvider());
+
+    }
 }
 
 
@@ -98,7 +181,6 @@ class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
 
     /**
      * 登录成功时会执行的逻辑
-     * 主要是验证账号名密码
      */
     @Override
     protected void successfulAuthentication(
@@ -120,6 +202,26 @@ class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
     }
 }
 
+/**
+ * jwt验证过滤器,用于验证jwt是否合法
+ */
+class JWTAuthenticationFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain filterChain)
+            throws IOException, ServletException {
+        Authentication authentication = TokenAuthenticationService
+                .getAuthentication((HttpServletRequest) request);
+
+        SecurityContextHolder.getContext()
+                .setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+}
+
+@Slf4j
 class TokenAuthenticationService {
     static final long EXPIRATIONTIME = 432_000_000;     // 5天
     static final String SECRET = "P@ssw02d";            // JWT密码
@@ -139,11 +241,15 @@ class TokenAuthenticationService {
                 .signWith(SignatureAlgorithm.HS512, SECRET)
                 .compact();
 
+        log.info("jwt token 生成成功 token : {}", JWT);
         // 将 JWT 写入 body
         try {
             response.setContentType("application/json");
             response.setStatus(HttpServletResponse.SC_OK);
-            response.getOutputStream().println(String.valueOf(ImmutableMap.of("token", JWT)));
+            JSONObject token = new JSONObject() {{
+                put("token", JWT);
+            }};
+            response.getOutputStream().println(token.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -163,16 +269,88 @@ class TokenAuthenticationService {
                     .getBody();
 
             // 拿用户名
-            String user = claims.getSubject();
+            String username = claims.getSubject();
 
             // 得到 权限（角色）
             List<GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList((String) claims.get("authorities"));
 
             // 返回验证令牌
-            return user != null ?
-                    new UsernamePasswordAuthenticationToken(user, null, authorities) :
+            return username != null ?
+                    new UsernamePasswordAuthenticationToken(username, null, authorities) :
                     null;
         }
         return null;
     }
+}
+
+// 自定义身份认证验证组件
+class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        // 获取认证的用户名 & 密码
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        // 认证逻辑
+        if (name.equals("admin") && password.equals("123456")) {
+
+            // 这里设置权限和角色
+            ArrayList<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+            authorities.add(new SimpleGrantedAuthority("AUTH_WRITE"));
+            // 生成令牌
+            Authentication auth = new UsernamePasswordAuthenticationToken(name, password, authorities);
+            return auth;
+        } else {
+            throw new BadCredentialsException("密码错误~");
+        }
+    }
+
+    // 是否可以提供输入类型的认证服务
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+}
+
+/**
+ * <code>@WebFilter</code>将一个实现了javax.servlet.Filter接口的类定义为过滤器
+ * 属性filterName声明过滤器的名称,可选
+ * 属性urlPatterns指定要过滤 的URL模式,也可使用属性value来声明.(指定要过滤的URL模式是必选属性)
+ * <code>@Order</code>指定过滤器的执行顺序,值越大越先执行
+ */
+@Component
+@WebFilter(filterName = "MyCorsFilter", urlPatterns = "/*")
+@Order(2)
+class MyCorsFilter implements Filter {
+
+    /**
+     * 执行过滤操作
+     */
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        // 设置响应头的权限信息 request.getHeader("Origin")
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void init(FilterConfig config) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+本项目为spring-boot 简单教程项目
+
+## 目录
+### json-web-token 
+使用spring security 结合jwt 组件完成jwt的验证 
+### mongoDb
+使用spring data mongodb 完成对 mongodb的操作
