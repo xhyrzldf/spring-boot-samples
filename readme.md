9327b539af00ee96d8c4e6b7818adb86f429b4d9
xhyrzldf <xhyrzldf@gmail.com>
2018-08-17 12:16:57 +0800

    spring-security提交

--- readme.md
+++ readme.md
@@ -6,3 +6,14 @@
 ### mongoDb
 使用spring data mongodb 完成对 mongodb的操作
 ### spring security
+提供以下功能
+- 认证
+- 授权
+- 攻击防护
+
+#### 基本原理
+基于过滤器链来进行验证
+- Username Password Authentication Filter
+- Basic Authentication Filter
+- Exception Translation Filter 处理spring security异常
+- Filter Security Interceptor
--- spring-security/src/main/java/com/zjty/security/SpringSecurityApplication.java
+++ spring-security/src/main/java/com/zjty/security/SpringSecurityApplication.java
@@ -2,11 +2,37 @@ package com.zjty.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+}
+
+@RestController
+class Hello {
+
+    @GetMapping("/hello")
+    public String HelloWorld() {
+        return "Hello Spring Security";
+    }
+}
+
+@Configuration
+class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
     }
 }
